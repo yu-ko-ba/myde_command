0b68dd6a359ec1698fc788117a2c0e7f1431ac77
yu-ko-ba <n74_cjpxjljg.tcnp7jy@icloud.com>
2020-03-01 14:42:06 +0900

    引数にファイルのパスを与えた場合、コンテナ内のneovimが起動するようにした ディレクトリのパスを与えた場合は、そのディレクトリがマウントされるようにした

--- myde.sh
+++ myde.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run -it --rm -v $(pwd):/home/Yu/work yukoba0616/myde
+if [ "${1:0:1}" == "/" ]; then
+  dir_path=$(dirname "${1}")
+else
+  dir_path=$(pwd)/$(dirname "${1}")
+fi
+
+if [ -d "${1}" -o "${1}" == "" ]; then
+  docker run -it --rm -v ${dir_path}/$(basename "${1}"):/home/Yu/work yukoba0616/myde
+else
+  docker run -it --rm -v ${dir_path}:/home/Yu/work yukoba0616/myde nvim $(basename "${1}")
+fi
